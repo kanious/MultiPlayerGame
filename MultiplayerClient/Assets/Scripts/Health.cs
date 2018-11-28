64b9b943430edae5d5ad55b62a4c3f1047521ec8
Jihye <kanious@naver.com>
2018-11-28 10:31:20 +0900

    no message

---
+++ MultiplayerClient/Assets/Scripts/Health.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public const int maxHealth = 100;
+    public bool destroyOnDeath = false;
+
+    public int currentHealth = maxHealth;
+    public bool isEnemy = false;
+
+    public RectTransform healthBar;
+
+    private bool isLocalPlayer;
+
+	void Start () {
+        PlayerController pc = GetComponent<PlayerController>();
+        isLocalPlayer = pc.isLocalPlayer;
+	}
+
+    public void TakeDamage(GameObject playerFrom, int amount)
+    {
+        currentHealth -= amount;
+        OnChangeHealth();
+
+        NetworkManager._instance.GetComponent<NetworkManager>().CommandHealthChange(
+            playerFrom, this.gameObject, amount, isEnemy);
+    }
+
+    public void OnChangeHealth()
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+                Destroy(gameObject);
+            else
+            {
+                currentHealth = maxHealth;
+                healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+                Respawn();
+            }
+        }
+    }
+
+    private void Respawn()
+    {
+        if(isLocalPlayer)
+        {
+            Vector3 spawnPoint = Vector3.zero;
+            Quaternion spawnRotation = Quaternion.Euler(0f, 180f, 0f);
+            transform.position = spawnPoint;
+            transform.rotation = spawnRotation;
+        }
+    }
+}
---
+++ MultiplayerClient/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public bool isLocalPlayer = false;
+
+    Vector3 oldPosition;
+    Vector3 currentPosition;
+    Quaternion oldRotation;
+    Quaternion currentRotation;
+
+	void Start () {
+        oldPosition = transform.position;
+        currentPosition = oldPosition;
+        oldRotation = transform.rotation;
+        currentRotation = oldRotation;
+	}
+
+    void OnApplicationQuit()
+    {
+        if (isLocalPlayer)
+        {
+            NetworkManager._instance.GetComponent<NetworkManager>().CommandQuit();
+        }
+    }
+
+    void Update () {
+
+        if (!isLocalPlayer)
+            return;
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3f;
+
+        transform.Rotate(0f, x, 0f);
+        transform.Translate(0f, 0f, z);
+
+        currentPosition = transform.position;
+        currentRotation = transform.rotation;
+
+        if (currentPosition != oldPosition)
+        {
+            NetworkManager._instance.GetComponent<NetworkManager>().CommandMove(transform.position);
+            oldPosition = currentPosition;
+        }
+
+        if(currentRotation != oldRotation)
+        {
+            NetworkManager._instance.GetComponent<NetworkManager>().CommandRotate(transform.rotation);
+            oldRotation = currentRotation;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            NetworkManager._instance.GetComponent<NetworkManager>().CommandShoot();
+        }
+	}
+
+    public void CmdFire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation) as GameObject;
+        Bullet b = bullet.GetComponent<Bullet>();
+        b.playerFrom = this.gameObject;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.up * 6f;
+
+        Destroy(bullet, 2f);
+    }
+
+}
